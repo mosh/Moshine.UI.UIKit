baac876ea0b5f3a04271c8f7da757c295aadc280
John Moshakis <john@moshakis.com>
2016-12-20 23:03:19 -0500

    date picker

---
+++ MoshineDatePickerTableViewCell.pas
@@ -0,0 +1,35 @@
+﻿namespace Moshine.UI.UIKit;
+
+uses
+  Foundation, UIKit;
+
+type
+
+  [IBObject]
+  MoshineDatePickerTableViewCell = public class(MoshineBaseTableViewCell)
+  private
+  protected
+  
+    method get_DatePicker:UIDatePicker;
+    begin
+      exit cellControl as UIDatePicker;
+    end;
+  
+  public
+  
+    method createControl:UIView; override;
+    begin
+      exit new UIDatePicker;
+    end;
+  
+    method setup; override;
+    begin
+      inherited setup;
+      
+    end;
+    
+    property DatePicker:UIDatePicker read get_DatePicker;
+  
+  end;
+
+end.
