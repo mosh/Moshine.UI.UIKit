38346912cafd80327776a004ea84439dff3d5db5
John Moshakis <JohnMoshakis@john-moshakiss-macbook-pro.local>
2017-08-22 22:20:40 -0400

    Fix for font size issue

--- MoshineTextViewTableViewCell.pas
+++ MoshineTextViewTableViewCell.pas
@@ -8,48 +8,50 @@ type
   [IBObject]
   MoshineTextViewTableViewCell = public class(MoshineBaseTableViewCell, IUITextViewDelegate)
   private
-  
+
   protected
-  
+
     method createControl:UIView; override;
     begin
       exit new UITextView;
     end;
-    
+
     method get_textView:UITextView;
     begin
       exit cellControl as UITextView;
     end;
-  
+
   public
-  
+
     method setup; override;
     begin
       inherited setup;
-      
+
       textView.delegate := self;
+      textView.setFont(UIFont.systemFontOfSize(17));
+
     end;
-    
+
     [IBOutlet]property textView:weak UITextView read get_textView;
-  
-    
+
+
     method touchesBegan(touches: not nullable NSSet) withEvent(&event: nullable UIEvent); override;
     begin
       self.textView:becomeFirstResponder;
     end;
-    
+
     method textViewDidChange(textView: UITextView);
     begin
       if (assigned(OnTextChanged)) then
       begin
         OnTextChanged(textView.text);
       end;
-      
+
     end;
-    
+
     property OnTextChanged:OnTextChangedDelegate;
 
-  
+
   end;
 
 end.
