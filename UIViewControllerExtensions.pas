41e40bb4fee800411ff0e2264356fcb65fba26e1
John Moshakis <john.moshakis@firmex.com>
2019-11-02 19:59:46 -0400

    method for creating search

---
+++ UIViewControllerExtensions.pas
@@ -0,0 +1,32 @@
+﻿namespace Moshine.UI.UIKit;
+
+uses
+  UIKit;
+
+type
+  UIViewControllerExtensions = public extension class(UIViewController)
+  public
+
+    method createSearchController(placeHolder:String) withTableView(someTableView:UITableView):UISearchController;
+    begin
+      var searchController := new UISearchController withSearchResultsController(nil);
+      searchController.searchBar.placeholder := placeHolder;
+      //searchController.delegate := self;
+      //searchController.searchResultsUpdater := self;
+      searchController.dimsBackgroundDuringPresentation := false;
+
+      if available("iOS 11.0") then
+      begin
+        self.navigationItem.searchController :=  searchController;
+      end
+      else
+      begin
+        someTableView.tableHeaderView := searchController.searchBar;
+      end;
+
+      exit searchController;
+
+    end;
+  end;
+
+end.
